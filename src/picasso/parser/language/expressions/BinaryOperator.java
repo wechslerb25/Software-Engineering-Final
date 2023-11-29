e0f12156c4932511228f63c3c110bf8fb9d23d93
Reese Nelson <nelsonr25@mail.wlu.edu>
2023-11-28 23:10:31 -0500

    Added toString and equals for BinaryOperator abstract class.

--- src/picasso/parser/language/expressions/BinaryOperator.java
+++ src/picasso/parser/language/expressions/BinaryOperator.java
@@ -25,6 +25,40 @@ public abstract class BinaryOperator extends ExpressionTreeNode{
 		this.param2 = param2;
 	}
 	
-	//add more functions
+	/**
+	 * Returns the string representation of the operation in the format 
+	 * "<ClassName>: <parameter1>, <parameter2>"
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		String classname = this.getClass().getName();
+		return classname.substring(classname.lastIndexOf(".") + 1) + ": " + param1 + ", " + param2;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
 
+		if (!(o instanceof BinaryOperator)) {
+			return false;
+		}
+
+		// Make sure the objects are the same type
+
+		if (o.getClass() != this.getClass()) {
+			return false;
+		}
+
+		BinaryOperator b = (BinaryOperator) o;
+
+		// check if their parameters are equal
+		if (!this.param1.equals(b.param1) || !this.param2.equals(b.param2)) {
+			return false;
+		}
+		return true;
+	}
 }
