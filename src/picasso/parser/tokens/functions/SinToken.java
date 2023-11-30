18c6be983e0ea81de95f38dbdd428ae76dc2d2c7
Ben Wechsler <Wechslerb25@mail.wlu.edu>
2023-11-30 09:38:41 -0500

    Added token for the Sin function

---
+++ src/picasso/parser/tokens/functions/SinToken.java
@@ -0,0 +1,12 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the sin function token
+ * 
+ */
+public class SinToken extends FunctionToken {
+
+	public SinToken() {
+		super("Sin Function Token");
+	}
+}
