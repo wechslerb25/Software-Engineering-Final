1ceb92f576f795febb5af73246023c694f5c9187
Ben Wechsler <Wechslerb25@mail.wlu.edu>
2023-12-04 10:20:56 -0500

    Adds an if statement that will read a line from a file if the file is a txt

--- src/picasso/view/commands/Reader.java
+++ src/picasso/view/commands/Reader.java
@@ -4,6 +4,7 @@ import javax.swing.JFileChooser;
 
 import picasso.model.Pixmap;
 import picasso.util.FileCommand;
+import java.util.Scanner;
 
 /**
  * Open the chosen image file and display in the Pixmap target.
@@ -22,10 +23,29 @@ public class Reader extends FileCommand<Pixmap> {
 	/**
 	 * Displays the image file on the given target.
 	 */
+	String expression = null;
 	public void execute(Pixmap target) {
 		String fileName = getFileName();
-		if (fileName != null) {
-			target.read(fileName);
-		}
+	    if (fileName != null) {
+		    int PeriodIndex = fileName.lastIndexOf(".");  //gets the index of the last occurrence of a period in the string
+		    String NewString = fileName.substring(PeriodIndex + 1); //slices the string from the period to the end exclusively
+		    NewString = NewString.toLowerCase();
+		    System.out.println(NewString);
+		    
+	    	if (NewString.equals("txt")) {
+	    		System.out.println(fileName);
+	    		Scanner scan = new Scanner(fileName);
+	    		expression = scan.nextLine();
+	    		System.out.println(expression);
+	    		scan.close();
+	    	}
+	    	
+	    	else {
+	    		target.read(fileName);
+	    	}
+	    }
+	}
+	public String getExpression() {
+		return expression;
 	}
 }
