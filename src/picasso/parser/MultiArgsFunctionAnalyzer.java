a25310eb2d559dcf5d347decd2394f6fb15a5a73
Janjanjan1 <janeetbaj@gmail.com>
2023-12-05 10:26:33 -0500

    evaluator works with enter beginning to work on if

---
+++ src/picasso/parser/MultiArgsFunctionAnalyzer.java
@@ -0,0 +1,24 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.RGBColor;
+import picasso.parser.tokens.Token;
+
+/**
+ * Parses a function that takes one expression as a parameter.
+ * 
+ * XXX: Not sure if this class is necessary.
+ * 
+ * @author Sara Sprenkle
+ * 
+ */
+public abstract class MultiArgsFunctionAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public abstract ExpressionTreeNode generateExpressionTree(
+			Stack<Token> tokens);
+
+
+}
