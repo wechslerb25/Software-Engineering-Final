00ef861417450352e2593ccbef0deb90927ce7a0
Reese Nelson <nelsonr25@mail.wlu.edu>
2023-12-06 09:18:20 -0500

    Worked on conditional (wrong branch. oops)

--- src/picasso/parser/language/expressions/Conditional.java
+++ src/picasso/parser/language/expressions/Conditional.java
@@ -2,16 +2,35 @@ package picasso.parser.language.expressions;
 
 import picasso.parser.language.ExpressionTreeNode;
 
+/**
+* Represents the exp function in the Picasso language.
+* @author Reese Nelson
+*/
 public class Conditional extends TernaryFunction{
-
+	
+	/**
+	 * Create a conditional function that takes as a parameter the given expressions
+	 * 
+	 * @param cond the condition to evaluate
+	 * @param t the expression to evaluate if the condition is true
+	 * @param f the expression to evaluate if the condition is false
+	 */
 	public Conditional(ExpressionTreeNode cond, ExpressionTreeNode t, ExpressionTreeNode f) {
 		super(cond, t, f);
 	}
 
 	@Override
 	public RGBColor evaluate(double x, double y) {
-		// TODO Auto-generated method stub
+		RGBColor resultCond = param1.evaluate(x, y);
+		RGBColor resultT = param2.evaluate(x, y);
+		RGBColor resultF = param3.evaluate(x, y);
+		
+		
+		/*double red = Math.exp(result.getRed());
+		double green = Math.exp(result.getGreen());
+		double blue = Math.exp(result.getBlue());
+
+		return new RGBColor(red, green, blue);*/
 		return null;
 	}
-
 }
