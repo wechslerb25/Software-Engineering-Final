d3fccb2406b3ca3ec3d2bbee870ab9254fdcdc81
hallidayt24 <hallidayt24@mail.wlu.edu>
2023-12-06 08:54:46 -0500

    Added Log Function

---
+++ src/picasso/parser/language/expressions/Log.java
@@ -0,0 +1,21 @@
+package picasso.parser.language.expressions;
+
+import picasso.parser.language.ExpressionTreeNode;
+
+public class Log extends UnaryFunction {
+	
+	public Log(ExpressionTreeNode param) {
+		super(param);
+	}
+	
+	@Override
+	public RGBColor evaluate(double x, double y) {
+		RGBColor result = param.evaluate(x, y);
+		double red = Math.log(result.getRed());
+		double green = Math.log(result.getGreen());
+		double blue = Math.log(result.getBlue());
+
+		return new RGBColor(red, green, blue);
+	}
+
+}
