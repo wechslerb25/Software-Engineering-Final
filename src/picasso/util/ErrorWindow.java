45658036f4db4165a80e703ac9883bf1df0adb15
Reese Nelson <nelsonr25@mail.wlu.edu>
2023-12-08 08:01:25 -0500

    Added Error Window file.

---
+++ src/picasso/util/ErrorWindow.java
@@ -0,0 +1,26 @@
+package picasso.util;
+
+import javax.swing.JOptionPane;
+
+public class ErrorWindow {
+
+    public static void main(String[] args) {
+        try {
+            // code that might throw an exception; placeholder for final implementation 
+            int result = divide(10, 0);
+            System.out.println("Result: " + result);
+        } catch (Exception e) {
+            // display the error in a pop-up window
+            showErrorPopup("Error: " + e.getMessage());
+        }
+    }
+    
+    // adjust this for picasso equations and
+    private static int divide(int numerator, int denominator) {
+        return numerator / denominator;
+    }
+    // window that shows error message
+    private static void showErrorPopup(String errorMessage) {
+        JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+}
