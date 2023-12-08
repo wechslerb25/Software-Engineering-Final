b4621df9fad1d73207cbb64b3e86ca991c80a69e
Reese Nelson <nelsonr25@mail.wlu.edu>
2023-12-08 16:33:13 -0500

    Converted TernaryFunction dependencies to Function dependencies.

--- src/picasso/parser/language/expressions/ImageWrap.java
+++ src/picasso/parser/language/expressions/ImageWrap.java
@@ -3,10 +3,6 @@
  */
 package picasso.parser.language.expressions;
 
-import java.awt.Color;
-import java.awt.Dimension;
-
-import picasso.model.Pixmap;
 import picasso.parser.language.ExpressionTreeNode;
 
 /**
@@ -14,15 +10,15 @@ import picasso.parser.language.ExpressionTreeNode;
  * 
  * @author
  */
-public class ImageWrap extends TernaryFunction {
+public class ImageWrap extends Function {
 
     /**
      * Create a multiplication expression that takes as a parameter the given
      * expressions
      * 
-     * @param exp1 the name of the file
-     * @param exp2 the param for the x coordinate formula.
-     * @param exp3 the param for the y coordinate formula.
+     * @param param1 the name of the file
+     * @param param2 the param for the x coordinate formula.
+     * @param param3 the param for the y coordinate formula.
      */
     public ImageWrap(ExpressionTreeNode param1, ExpressionTreeNode param2, ExpressionTreeNode param3) {
         super(param1, param2, param3);
@@ -30,9 +26,9 @@ public class ImageWrap extends TernaryFunction {
 
     @Override
     public RGBColor evaluate(double x, double y) {
-        RGBColor result2 = param2.evaluate(x, y);
-        RGBColor result3 = param3.evaluate(x, y);
-        RGBColor result1 = param1.evaluate(result2.getBlue(), result3.getBlue());
+        RGBColor result2 = params.get(0).evaluate(x, y);
+        RGBColor result3 = params.get(1).evaluate(x, y);
+        RGBColor result1 = params.get(2).evaluate(result2.getBlue(), result3.getBlue());
         return result1;
     }
 
