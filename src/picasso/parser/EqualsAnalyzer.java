d205efb3c8cf69141d3cb7495fea66c5b4bbd67b
Reese Nelson <nelsonr25@mail.wlu.edu>
2023-12-09 19:58:38 -0500

    Fixed merge conflict.

--- src/picasso/parser/EqualsAnalyzer.java
+++ src/picasso/parser/EqualsAnalyzer.java
@@ -22,28 +22,11 @@ public class EqualsAnalyzer implements SemanticAnalyzerInterface {
 		if (tokens.empty()) {
 			throw new ParseException("Invalid Comparison. Comparison failed as right expression is empty.");
 		}
-<<<<<<< HEAD
 		ExpressionTreeNode exp2 = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
 		if (tokens.empty()) {
 			throw new ParseException("Invalid Comparison. Comparison failed as left expression is empty.");
-=======
-		if(!(tokens.peek() instanceof IdentifierToken)) {
-			throw new ParseException("Cannot assign non-Identifier Type.");
 		}
-		IdentifierToken it = (IdentifierToken) tokens.pop();
-		if (it == null) {
-			throw new ParseException("Empty Assignment. Assignment failed as expression assigned to nothing.");
->>>>>>> refs/heads/main
-		}
-<<<<<<< HEAD
 		ExpressionTreeNode exp1 = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
 		return new Equals(exp1, exp2);
-=======
-		if(it.getName().equals("x") || it.getName().equals("y")  ) {
-			throw new ParseException("Cannot assign reserved Identifiers.");
-		}
-		IdentifierAnalyzer.idToExpression.put(it.getName(), exp1);
-		return exp1;
->>>>>>> refs/heads/main
 	}
 }
