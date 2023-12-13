5d7104cbd58e1830c349732304167477b967c0d2
Reese Nelson <nelsonr25@mail.wlu.edu>
2023-12-12 19:46:01 -0500

    Added silence functionality for running unit tests.

--- src/picasso/util/ErrorWindow.java
+++ src/picasso/util/ErrorWindow.java
@@ -3,23 +3,17 @@ package picasso.util;
 import javax.swing.JOptionPane;
 
 public class ErrorWindow {
-
-    public static void main(String[] args) {
-        try {
-            // code that might throw an exception; placeholder for final implementation 
-            int result = divide(10, 0);
-        } catch (Exception e) {
-            // display the error in a pop-up window
-            showErrorPopup("Error: " + e.getMessage());
-        }
+	
+	private static boolean silenced = false;
+    
+    public static void setSilenced(boolean s) {
+    	silenced = s;
     }
     
-    // adjust this for picasso equations and
-    private static int divide(int numerator, int denominator) {
-        return numerator / denominator;
-    }
     // window that shows error message
     public static void showErrorPopup(String errorMessage) {
-        JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+    	if(!silenced) {
+    		JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+    	}
     }
 }
