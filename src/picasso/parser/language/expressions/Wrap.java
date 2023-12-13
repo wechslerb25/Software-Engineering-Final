af2b558575cc4343b24d3cd4fab3eb42bebcfb7f
Ben Wechsler <Wechslerb25@mail.wlu.edu>
2023-12-13 15:30:07 -0500

    Added wrap function and testing

--- src/picasso/parser/language/expressions/Wrap.java
+++ src/picasso/parser/language/expressions/Wrap.java
@@ -15,24 +15,25 @@ public class Wrap extends AbstractFunction {
 		if (red>1.0) {
 			red = red-2;
 		}
-		if (red<1.0) {
+		if (red<-1.0) {
 			red = red+2;
 		}
 		double green = result.getGreen();
 		if (green>1.0) {
 			green = green-2;
 		}
-		if (green<1.0) {
+		if (green<-1.0) {
 			green = green+2;
 		}
 		double blue = result.getBlue();
 		if (blue>1.0) {
 			blue = blue-2;
 		}
-		if (blue<1.0) {
+		if (blue<-1.0) {
 			blue = blue+2;
 		}
 		return new RGBColor(red, green, blue);
 	}
-	
 }
+
+		
